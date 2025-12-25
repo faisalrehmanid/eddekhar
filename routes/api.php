8a0690935913d323a023047e0c54e9bff3fdab27
Faisal Rehman <faisalrehmanid@hotmail.com>
2025-12-25 15:21:37 +0500

    register api.php routes and added health check service

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application.
+| These routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group.
+|
+*/
+
+// Health check endpoint
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok']);
+});
