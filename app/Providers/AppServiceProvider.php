02df4a3068bc761e9fb877aa605f4099ecf33c77
Faisal Rehman <faisalrehmanid@hotmail.com>
2025-12-25 21:26:23 +0500

    Setting up DB configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Database
+        $this->app->singleton(\App\Library\Db\DB::class, function ($app) {
+            $db_config = [
+                'hostname' => env('DB_HOST'),
+                'port' => env('DB_PORT'),
+                'username' => env('DB_USERNAME'),
+                'password' => env('DB_PASSWORD'),
+                'database' => env('DB_DATABASE'),
+                'driver' => 'pdo_mysql',
+                'charset' => 'utf8mb4',
+            ];
+
+            return new \App\Library\Db\DB($db_config);
+        });
     }
 
     /**
